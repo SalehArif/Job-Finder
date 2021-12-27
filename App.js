10245c1f75ad5a8637ae3c1ef4335452bcec92ad
anand <anand_talreja1@hotmail.com>
2021-12-27 15:40:30 +0500

    Firebase Integrated and Loigin register created

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   View,
@@ -7,13 +8,17 @@ import {
   StyleSheet,
   Dimensions,
   TouchableOpacity,
+  TextInput,
+  Button
 } from "react-native";
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
 const Stack = createNativeStackNavigator();
 
-const InitialScreen = () => {
+const InitialScreen = ({navigation}) => {
   return (
     <View style={{backgroundColor:'#1C58F2',width:'100%',height:'100%',justifyContent:'center',alignItems:'center'}}>
       
@@ -32,13 +37,13 @@ const InitialScreen = () => {
       
       <View>
         <TouchableOpacity
-          
+         onPress={()=>{navigation.navigate('Register')}} 
         >
           <Text style={[stylesheet.button_initial,{backgroundColor:'#42EAB7',color:'#344161'}]}>Register Now</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
-          
+          onPress={()=>{navigation.navigate('Login')}} 
         >
           <Text style={[stylesheet.button_initial,{backgroundColor:'#1A1928',color:'white'}]}>Login</Text>
         </TouchableOpacity>
@@ -47,11 +52,148 @@ const InitialScreen = () => {
     </View>
   );
 };
+
+const Registeration = ()=>{
+  const [email,setemail]=React.useState('');
+  const [password,setpassword] = React.useState('');
+  const [repeatpass,setrepeatpass] = React.useState('');
+  const [type,settype]=React.useState('Employee');
+  
+  const postdata = ()=>{
+    if(password===repeatpass)
+    {
+      var requestopt = {
+        method:"POST",
+        body:JSON.stringify({  
+          emp_email:email,
+          emp_pass:password,
+
+          },
+        )
+      };
+      fetch("https://jobfinder-80af8-default-rtdb.firebaseio.com/Employees.json",requestopt)
+      .then((response)=>response.json)
+      .then((result)=>{
+        console.log(result)
+        alert("User Successfully created");
+      })
+      .catch((error)=>console.log('error : ',error));
+    }
+    else
+    {
+      alert("repeat pass does not match");
+    }
+  }
+
+  return (
+    <View style={{width:'100%',height:'100%',justifyContent:'center',alignItems:'center'}}>
+      <Text style={stylesheet.heading}>Sign Up</Text>
+      <TouchableOpacity
+      onPress={()=>{type=="EMPLOYEE"?settype('EMPLOYEER'):settype("EMPLOYEE")}}
+      
+      >
+        <Text style={{fontSize:40,color:'black',marginBottom:40}}>{type}</Text>
+      </TouchableOpacity>
+      <TextInput style={[stylesheet.input]} onChangeText={(val)=>{setemail(val)}} placeholder="e-mail" />
+      <TextInput style={[stylesheet.input]} onChangeText={(val)=>{setpassword(val)}} placeholder="password" />
+      <TextInput style={[stylesheet.input]} onChangeText={(val)=>{setrepeatpass(val)}} placeholder="repeat password" />
+      <TouchableOpacity
+        onPress={()=>{
+          postdata();
+        }}
+        >
+          <Text style={[stylesheet.button_initial,{backgroundColor:'#1C58F2',color:'#ffff'}]}>Sign up</Text>
+        </TouchableOpacity>
+    </View>
+  )
+}
+
+const Login = ()=>{
+  const [email,setemail]=React.useState('');
+  const [password,setpassword] = React.useState('');
+  
+  const getdata = async ()=>{
+    const res  = await fetch("https://jobfinder-80af8-default-rtdb.firebaseio.com/Employees.json")
+    const data = await res.json();
+    return data;
+  }
+  const authenticate = async ()=>{
+    const data =await  getdata();
+    for ( key in data)
+    {
+      if (data[key].emp_email==email)
+      {
+        if( data[key].emp_pass==password)
+        {
+          alert("Authenticated");
+        }
+        else
+        {
+          alert("pasword does not match");
+        }
+      }
+      
+    }
+  }
+  
+  return (
+    <View style={{width:'100%',height:'100%',justifyContent:'center',alignItems:'center'}}>
+      <Text style={stylesheet.heading}>Login</Text>
+      <TextInput style={[stylesheet.input]} onChangeText={(val)=>{setemail(val)}} placeholder="e-mail" />
+      <TextInput style={[stylesheet.input]} onChangeText={(val)=>{setpassword(val)}} placeholder="password" />
+      
+      <TouchableOpacity
+         onPress={()=>{
+          authenticate();
+         }}
+        >
+          <Text style={[stylesheet.button_initial,{backgroundColor:'#1C58F2',color:'#ffff'}]}>Login</Text>
+        </TouchableOpacity>
+    </View>
+  )
+}
+
+
+//toremove
+const FirebaseCheck = () =>{
+
+
+  const postdata = ()=>{
+    var requestopt = {
+      method:"POST",
+      body:JSON.stringify({  
+        emp_email:'newasd@mail.com',
+        emp_pass:'testing123',
+
+        },
+      )
+    };
+    fetch("https://jobfinder-80af8-default-rtdb.firebaseio.com/Employees.json",requestopt)
+    .then((response)=>response.json)
+    .then((result)=>console.log(result))
+    .catch((error)=>console.log('error : ',error));
+  }
+
+  const getdata = async ()=>{
+    const res  = await fetch("https://jobfinder-80af8-default-rtdb.firebaseio.com/Employees.json")
+    const data = await res.json();
+    console.log(data);
+  }
+  return(
+    <View>
+      <Button title="Get Data" onPress={()=>{getdata()}} />
+      <Button title="POST Data" onPress={()=>{postdata()}} />
+    </View>
+  )
+}
 const App = () => {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={InitialScreen}  />
+    <Stack.Navigator initialRouteName="Homee">
+    {/* <Stack.Screen  name = "Firebase" component={FirebaseCheck} /> */}
+      <Stack.Screen name="Homee" component={InitialScreen}  />
+      <Stack.Screen name="Register" component={Registeration}  />
+      <Stack.Screen name="Login" component={Login}  />
     </Stack.Navigator>
     </NavigationContainer>
   );
@@ -73,10 +215,27 @@ const stylesheet = StyleSheet.create({
     textAlign:"center",
     justifyContent:'center',
     marginBottom:10,
-    
-
 
   },
+  heading:{
+    width:250,
+    height:80,
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign:'center',
+    fontSize:60,
+    marginBottom:150,
+    color:'#bdcbe1'
+  },
+  input:{
+    backgroundColor:'#bdcbe1',
+    borderRadius:15,
+    width:200,
+    height:60,
+    textAlign:'center',
+    margin:10,
+    fontSize:20,
+  }
 
 });
 
---
+++ test.js
@@ -0,0 +1,16 @@
+let val = {
+    "-MrvKBqw_tjk0DFQkiTl":  {
+      "emp_email": "asd@mail.com",
+      "emp_id": 7106481407803834,
+      "emp_pass": "testing123",
+    },
+    "-MrvKV1CUA-OfZcOpuSt":  {
+      "emp_email": "newasd@mail.com",
+      "emp_id": 8567263303653561,
+      "emp_pass": "testing123",
+    },
+  }
+for ( a in val)
+{
+    console.log(val[a].emp_email);
+}
