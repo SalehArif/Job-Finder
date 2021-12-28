becc063b628ce43db280c067ebac43ca0687787d
SalehArif <saleharif209@gmail.com>
2021-12-28 18:14:28 +0500

    Added Component

---
+++ components/jobs_display.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View } from "react-native";
+
+const Jobs = () => {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+    },
+})
+
+export default Jobs;
